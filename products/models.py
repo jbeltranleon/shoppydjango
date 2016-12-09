012934d5a0c115b9529076b1b8d6cb357377f3aa
Jhonbeltran <jbeltranleon@gmail.com>
2016-12-08 21:00:41 -0500

    Agrego una funcion al modelo para que el me retorne el nombre en ves de ProductObject usamos __str__ la cual es una funcion predefinida de python

--- products/models.py
+++ products/models.py
@@ -8,3 +8,6 @@ class Product(models.Model):
     description = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+    	return self.name
