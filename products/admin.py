4c0c5226108df4a397e25b98c2eebe52604edb49
Jhonbeltran <jbeltranleon@gmail.com>
2016-12-08 21:11:41 -0500

    Agrego un decorador y una clase en el admin para poder desplegar mas atributos de los productos

--- products/admin.py
+++ products/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Product
 
 # Register your models here.
-admin.site.register(Product)
+# admin.site.register(Product)
+
+@admin.register(Product)
+class AdminProduct(admin.ModelAdmin):
+    list_display = ('name', 'category', 'description', 'price')
