ec2a440bb9321b540d7b1793217c34db93500e90
Jhonbeltran <jbeltranleon@gmail.com>
2016-12-08 21:16:29 -0500

    Agrego list_filter para poder en este caso listar por categoria

--- products/admin.py
+++ products/admin.py
@@ -6,4 +6,5 @@ from .models import Product
 
 @admin.register(Product)
 class AdminProduct(admin.ModelAdmin):
-    list_display = ('id', 'name', 'category', 'description', 'price')
+    list_display = ('id', 'name', 'category', 'description', 'price',)
+    list_filter = ('category',)
