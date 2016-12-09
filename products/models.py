9106233e4e61d0b1b44417763cc2f3a997e434df
Jhonbeltran <jbeltranleon@gmail.com>
2016-12-08 19:01:52 -0500

    cambio el nombre del modelo a singular

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Products(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
