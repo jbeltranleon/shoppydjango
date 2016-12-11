5e973d0232997577607db8769be1ffcf3b9648e5
Jhonbeltran <jbeltranleon@gmail.com>
2016-12-11 17:30:33 -0500

    uso loader, render y httpresponse para mostrar un html junto con los templates de django, ya me comunico con la base de datos

--- products/views.py
+++ products/views.py
@@ -1,6 +1,15 @@
+from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
+
+from .models import Product
 
 # Create your views here.
 
 def hello_world(request):
-	return render(request, 'index.html')
+    products = Product.objects.order_by('id')
+    template = loader.get_template('index.html')
+    context = {
+        'products' : products
+    }
+    return HttpResponse(template.render(context, request))
