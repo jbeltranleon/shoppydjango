961dcbe341641088aaf5e0761978b064aef28f2f
Jhonbeltran <jbeltranleon@gmail.com>
2017-01-11 17:51:02 -0500

    Registro el modelo en admin.py

--- Shoppy/settings.py
+++ Shoppy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'products.apps.ClientsConfig',
     'products.apps.ProductsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Client
 
-# Register your models here.
+@admin.register(Client)
+class AdminClient(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'email', 'address',)
+    list_filter = ('name',)
