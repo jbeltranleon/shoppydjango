4a5ddb7562fd6dc41ecdb67bd85afe381ebe0d53
Jhonbeltran <jbeltranleon@gmail.com>
2017-01-18 22:56:24 -0500

    Creo base de login, aun no registro usuarios

--- Shoppy/urls.py
+++ Shoppy/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('products.urls')),
+    url(r'^', include('clients.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+def authentication(request):
+    if request.method == 'POST':
+        action = request.POST.get('action', None)
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        if action == 'signup':
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+        elif action == 'login':
+            user = authenticate(username=username, password=password)
+            login(request, user)
+
+
+    return render(request, 'login.html', {})
