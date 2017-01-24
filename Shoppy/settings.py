878b62b597386b8f466237ad33181b8a4c386b12
Jhonbeltran <jbeltranleon@gmail.com>
2017-01-23 20:48:50 -0500

    Cambio de sqlite a postgresql ➜  ~ sudo -i -u postgres [sudo] password for jbeltranleon: postgres@jbeltranleon:~$ createdb shoppydb postgres@jbeltranleon:~$ psql psql (9.5.5) Type help for help.
    postgres=# \q
    postgres@jbeltranleon:~$ psql -d shoppydb
    psql (9.5.5)
    Type help for help.
    
    shoppydb=# GRANT ALL PRIVILEGES ON DATABASE shoppydb TO fredy;
    GRANT
    
    y despues
    ./manage.py migrate

--- Shoppy/settings.py
+++ Shoppy/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Shoppy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shoppydb',
+        'USER': 'fredy',
+        'PASSWORD': 'fredy123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
