af41dd9ed1989e8004ec81ac52ba21cf9e5bd2bb
Jhonbeltran <jbeltranleon@gmail.com>
2017-01-25 17:47:06 -0500

    Agrego enlace al logo

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.ProductList.as_view(), name='hello'),
+	url(r'^$', views.ProductList.as_view(), name='home'),
 	url(r'^product/(?P<pk>[0-9]+)/$', views.ProductDetail.as_view(), 
 		name='detail'),
 	url(r'^product/new', views.new_product, name='new_product')
