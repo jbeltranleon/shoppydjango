9b4805cbd03ab08f6b0658f0c945af195c555d2a
Jhonbeltran <jbeltranleon@gmail.com>
2017-03-11 20:57:20 -0500

    Agrego la primera Prueva a mi proyecto para usar TDD

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Client
 
-# Create your tests here.
+class ClientTestCase(TestCase):
+	def setUp(self):
+		Client.objects.create(name='Fredo',
+							  phone=23232312, 
+							  email='j@gmail.com', 
+							  address='Calle falsa 123')
+
+	def test_clients_have_email(self):
+		client = Client.objects.get(name="Fredo")
+
+		self.assertEqual(client.email, 'j@gmail.com')
